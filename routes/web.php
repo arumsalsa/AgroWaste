d6ddc5731a203bc254ad1192c96d8e2318c93d5a
arumsalsa <arumsalsabila027@gmail.com>
2026-06-12 14:43:28 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test', function () {
+    return response()->json(['status' => 'ok', 'routes' => 'working']);
 });
