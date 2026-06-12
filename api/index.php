a417d8dcbb8a1828697ce3a166b0d56f64725e19
arumsalsa <arumsalsabila027@gmail.com>
2026-06-12 14:34:59 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -16,17 +16,10 @@ putenv('APP_CONFIG_CACHE=/tmp/bootstrap/cache/config.php');
 putenv('APP_ROUTES_CACHE=/tmp/bootstrap/cache/routes.php');
 putenv('APP_EVENTS_CACHE=/tmp/bootstrap/cache/events.php');
 
-// Debug: lihat path yang masuk
-if (isset($_GET['debug'])) {
-    header('Content-Type: application/json');
-    echo json_encode([
-        'REQUEST_URI' => $_SERVER['REQUEST_URI'],
-        'PATH_INFO' => $_SERVER['PATH_INFO'] ?? null,
-        'SCRIPT_NAME' => $_SERVER['SCRIPT_NAME'],
-    ]);
-    exit;
+// Gunakan PATH_INFO sebagai REQUEST_URI
+if (!empty($_SERVER['PATH_INFO'])) {
+    $query = isset($_SERVER['QUERY_STRING']) ? '?' . $_SERVER['QUERY_STRING'] : '';
+    $_SERVER['REQUEST_URI'] = $_SERVER['PATH_INFO'] . $query;
 }
-
-$_SERVER['REQUEST_URI'] = preg_replace('#^/api#', '', $_SERVER['REQUEST_URI']) ?: '/';
 
 require __DIR__ . '/../public/index.php';
